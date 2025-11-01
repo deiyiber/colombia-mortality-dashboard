91d2a6ec5313afbb26fcb88a7fec72b7b66ec2e9
deiyiber <deiyiber@gmail.com>
2025-11-01 12:41:45 -0500

    ajuste en server

--- app.py
+++ app.py
@@ -183,8 +183,7 @@ app.layout = html.Div([
 
 #if __name__ == "__main__":
 ##   app.run(debug=True)
-
 if __name__ == "__main__":
-    # Solo se ejecuta si corres localmente (no en Render)
     port = int(os.environ.get("PORT", 8080))
-    app.run_server(host="0.0.0.0", port=port, debug=True)
+    app.run_server(host="0.0.0.0", port=port, debug=False)
+
