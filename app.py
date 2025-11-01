8da329dfa2f518c27ca8a8577ffa6ee7e791011b
deiyiber <deiyiber@gmail.com>
2025-11-01 13:09:28 -0500

    ajustes

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ app.layout = html.Div([
 
 #if __name__ == "__main__":
 ##   app.run(debug=True)
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run_server(host="0.0.0.0", port=port, debug=False)
+#if __name__ == "__main__":
+#    port = int(os.environ.get("PORT", 8080))
+#    app.run_server(host="0.0.0.0", port=port, debug=False)
 
